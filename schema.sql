0ab5581e0b0e96116079572efd223ae15f454377
Cecilia Goss <ceci.goss@gmail.com>
2017-11-04 15:29:15 -0500

    add index.html and dist directory

--- schema.sql
+++ schema.sql
@@ -4,13 +4,22 @@ CREATE DATABASE test;
 
 USE test;
 
-CREATE TABLE items (
+-- CREATE TABLE items (
+--   id int NOT NULL AUTO_INCREMENT,
+--   quantity integer NOT NULL,
+--   description varchar(50) NOT NULL,
+--   PRIMARY KEY (ID)
+-- );
+
+CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
-  quantity integer NOT NULL,
-  description varchar(50) NOT NULL,
-  PRIMARY KEY (ID)
+  name text NOT NULL,
+  password text NOT NULL
 );
 
+CREATE TABLE recipes (
+  id int NOT NULL AUTO_INCREMENT
+);
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
