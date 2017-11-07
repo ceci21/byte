ff2802bca110529ac66c5de1051eff43dde195b4
Cecilia Goss <ceci.goss@gmail.com>
2017-11-07 12:14:41 -0600

    create basic front-end with parallax image views

--- react-client/src/components/RecipeEntry_Test.jsx
+++ react-client/src/components/RecipeEntry_Test.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
+ CardSubtitle, CardBody } from 'reactstrap';
+ import { Parallax } from 'react-parallax';
 
-const RecipeEntry_Test = (props) => (
-  <div className="recipe-entry" id={props.id}>
-    <h4>{props.data.recipeName}</h4>
-    <img height="100" width="100" src={props.data.imageUrlsBySize["90"]}/>
-    <ul>{props.data.ingredients.map((element, index) => {return <li>{element}</li>})}</ul>
-  </div>
-);
+const RecipeEntry_Test = (props) => {
+  // Create new image link with higher resolution
+  var imgLink = props.data.imageUrlsBySize["90"];
+  var imgSizePropertyIndex = imgLink.indexOf("=s90-c");
+  imgLink = imgLink.slice(0, imgSizePropertyIndex) + "=s1080-c";
+  
+  // Limit title to 25 characters
+  if (props.data.recipeName.length > 32) {
+    var title = props.data.recipeName.slice(0, 32) + "...";
+  } else {
+    title = props.data.recipeName;
+  }
+  
+  var ingredientsArray = [];
+  for (var n = 0; n < props.data.ingredients.length; n++) {
+    if (props.data.ingredients[n].length > 32) {
+      ingredientsArray.push(props.data.ingredients[n].slice(0, 32) + "...");
+    } else {
+      ingredientsArray.push(props.data.ingredients[n]);
+    }
+  }
+  return (
+    <div className="recipe-entry" id={props.id}>
+      <CardGroup>
+        <Card>
+          <Parallax className="recipe-image" bgImage={imgLink} strength={100}></Parallax>
+          <CardBody>
+            <CardTitle>{title}</CardTitle>
+            <CardText>{ingredientsArray.map((element, index) => {
+              if (index < 3) {
+                return <li>{element}</li>
+              }
+            })}</CardText>
+            <Button>Favorite</Button>
+          </CardBody>
+        </Card>
+      </CardGroup>
+    </div>);
+}
+
+// const RecipeEntry_Test = (props) => (
+//   <div className="recipe-entry" id={props.id}>
+//     <h4>{props.data.recipeName}</h4>
+//     <img height="100" width="100" src={props.data.imageUrlsBySize["90"]}/>
+//     <ul>{props.data.ingredients.map((element, index) => {return <li>{element}</li>})}</ul>
+//   </div>
+// );
 
 export default RecipeEntry_Test;
--- react-client/src/components/Search.jsx
+++ react-client/src/components/Search.jsx
@@ -4,9 +4,9 @@ import RecipeListItem from './RecipeListItem.jsx';
 const Search = (props) => (
   <div className="search">
   <form>
-    <label>Search</label>
-    <input type="text" onChange={(e) => {props.setStore({query: e.target.value})}} />
-     <button onClick={props.clickHandler}>Search</button>
+    <div className="search-title">What's in your pantry?</div>
+    <input className="search-input" type="text" onChange={(e) => {props.setStore({query: e.target.value})}} />
+     <button className="search-button" onClick={props.clickHandler}>Search</button>
    </form>
   </div>
 )
