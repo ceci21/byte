0a2fd95aba600fed16b7b1c8e0f4512bb6c07fe6
Cecilia Goss <ceci.goss@gmail.com>
2017-11-07 15:59:56 -0600

    add scrollbar in entry functionality

--- react-client/src/components/RecipeEntry_Test.jsx
+++ react-client/src/components/RecipeEntry_Test.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Card, Button, CardImg, CardTitle, CardText, CardGroup,
- CardSubtitle, CardBody } from 'reactstrap';
- import { Parallax } from 'react-parallax';
- import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline';
+CardSubtitle, CardBody } from 'reactstrap';
+import { Parallax } from 'react-parallax';
+import TiHeartFullOutline from 'react-icons/lib/ti/heart-full-outline';
+
 
 const RecipeEntry_Test = (props) => {
   // Create new image link with higher resolution
   var imgLink = props.data.imageUrlsBySize["90"];
   var imgSizePropertyIndex = imgLink.indexOf("=s90-c");
   imgLink = imgLink.slice(0, imgSizePropertyIndex) + "=s1080-c";
-  
+
   // Limit title to 25 characters
   if (props.data.recipeName.length > 19) {
     var title = props.data.recipeName.slice(0, 19) + "...";
   } else {
     title = props.data.recipeName;
   }
-  
+
   var ingredientsArray = [];
   for (var n = 0; n < props.data.ingredients.length; n++) {
-    if (props.data.ingredients[n].length > 32) {
-      ingredientsArray.push(props.data.ingredients[n].slice(0, 32) + "...");
-    } else {
-      ingredientsArray.push(props.data.ingredients[n]);
+      if (props.data.ingredients[n].length > 32) {
+        ingredientsArray.push(<li>{props.data.ingredients[n].slice(0, 32) + "..."}</li>);
+      } else {
+        ingredientsArray.push(<li>{props.data.ingredients[n]}</li>);
+      }
     }
-  }
+
   return (
     <div className="recipe-entry" id={props.id}>
       <CardGroup>
@@ -33,9 +35,7 @@ const RecipeEntry_Test = (props) => {
           <CardBody>
             <CardTitle className="recipe-title">{title}</CardTitle>
             <CardText className="recipe-ingredients">{ingredientsArray.map((element, index) => {
-              if (index < 3) {
-                return <li>{element}</li>
-              }
+              return element;
             })}</CardText>
             <div className="center">
               <a href="google.com">
