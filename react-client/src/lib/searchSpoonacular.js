21d8a077e5207bd1670706d043299c7bf1911dfa
Ross Salge <rossnsalge@gmail.com>
2017-11-11 12:18:13 -0600

    Add API key to searhSpoonacular.js for deployment

--- react-client/src/lib/searchSpoonacular.js
+++ react-client/src/lib/searchSpoonacular.js
@@ -1,27 +1,26 @@
 import $ from 'jquery';
-import Keys from '../config/spoonacular.js';
+
 
 var searchSpoonacular = function(options, callback) {
   var query = options.ingredients.toString();
-  console.log("Query: ", query);
-    $.ajax({
-       url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients",
-       type: "GET",
-       data: {
-         ingredients: query,
-         fillIngredients: true,
-         ranking: 2,
-         number: 100,
-         onlyOpenLicense: false
-       },
-       headers: {'X-Mashape-Key': Keys.APP_KEY},
-       success: function(data) {
-         callback(data);
-       },
-       error: function(err) {
-         console.log('The following error occured while retrieving your recipes from Spoonacular: ', err);
-       }
-    });
+  $.ajax({
+    url: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients",
+    type: "GET",
+    data: {
+      ingredients: query,
+      fillIngredients: true,
+      ranking: 2,
+      number: 100,
+      onlyOpenLicense: false
+    },
+    headers: {'X-Mashape-Key': 'DbYLQPD1ZGmshuSTypfqnBmQ3aZfp1uieQ6jsn4a0BqErZxQmz'},
+    success: function(data) {
+      callback(data);
+    },
+    error: function(err) {
+      console.log('The following error occured while retrieving your recipes from Spoonacular: ', err);
+    }
+  });
 }
 
 export {searchSpoonacular};
