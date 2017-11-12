30bd029caeceb64f229f8b206e8d3c866e3a1058
Ross Salge <rossnsalge@gmail.com>
2017-11-11 20:02:42 -0600

    Prevents bug where login and signup modal are both open at same time

--- react-client/src/components/LoginSubmissionForm.jsx
+++ react-client/src/components/LoginSubmissionForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LoginSubmissionForm = (props) => {
-  return (<div style={{"text-align":"center", "padding-top": "2em"}}><form name="login" onSubmit={props.onLoginHandler}>
+  return (<div style={{"text-align":"center", "padding-top": "2em", "padding-bottom":"2em"}}><form name="login" onSubmit={props.onLoginHandler}>
     <input name="username" id="username" type="text" placeholder="Username"></input>
     <input name="password" id="password" type="password" placeholder="Password"></input>
     <button type="submit">Login</button>
--- react-client/src/components/SignupSubmissionForm.jsx
+++ react-client/src/components/SignupSubmissionForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SignupSubmissionForm = (props) => {
-  return (<div style={{"text-align":"center", "padding-top": "2em"}}><form name="signup" onSubmit={props.onSignupHandler}>
+  return (<div style={{"text-align":"center", "padding-top": "2em","padding-bottom":"2em"}}><form name="signup" onSubmit={props.onSignupHandler}>
     <input name="username" id="username" type="text" placeholder="Username"></input>
     <input name="password" id="password" type="password" placeholder="Password"></input>
     <button type="submit">Sign Up</button>
