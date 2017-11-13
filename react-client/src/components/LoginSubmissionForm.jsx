7b0935953e303f8d8c7d2b9efa47371e1286cd11
Cecilia Goss <ceci.goss@gmail.com>
2017-11-13 16:32:46 -0600

    refactor process pt. 1

--- react-client/src/components/LoginSubmissionForm.jsx
+++ react-client/src/components/LoginSubmissionForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {InputGroup, FormControl, Button} from 'react-bootstrap';
+import TiTimes from 'react-icons/lib/ti/times';
+
 
 const LoginSubmissionForm = (props) => {
   return (
     <div className="submission-form">
+      <a className="close-button" onClick={() => {props.setStore({modalLogin: false, failLogin: ''})}}><TiTimes /></a>
       <form name="login" onSubmit={props.onLoginHandler}>
-        <FormControl className="input-field" name="username" id="username" type="text" placeholder="Username"/>
-        <FormControl className="input-field" name="password" id="password" type="password" placeholder="Password"/>
+        <FormControl className="input-field" name="username" id="username" type="text" placeholder="Username" />
+        <FormControl className="input-field" name="password" id="password" type="password" placeholder="Password" />
         <Button type="submit">Log In</Button>
       </form>
     </div>
--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,28 +1,34 @@
+// Standard libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
+
+// API
+import { searchSpoonacular } from './lib/searchSpoonacular.js';
+import { searchYummly } from './lib/searchYummly.js';
+import { spoonacularTrivia } from './lib/spoonacularTrivia.js';
+
+// Data
+import DEFAULT_TAGS from './data/DEFAULT_TAGS.js';
+import SAMPLE_DATA from './data/SAMPLE_DATA.js';
+
+// Components
+import Footer from './components/Footer.jsx';
+import LoadingText from './components/LoadingText.jsx';
+import LoginSubmissionForm from './components/LoginSubmissionForm.jsx';
+import NavBar from './components/NavBar.jsx';
 import RecipeList from './components/RecipeList.jsx';
 import Search from './components/Search.jsx';
-import _Test from './_Test.jsx'; /* Feel free to remove me! */
-import { searchYummly } from './lib/searchYummly.js';
-import { searchSpoonacular } from './lib/searchSpoonacular.js';
-import { spoonacularTrivia } from './lib/spoonacularTrivia.js';
-import SAMPLE_DATA from './data/SAMPLE_DATA.js';
-import DEFAULT_TAGS from './data/DEFAULT_TAGS.js';
-import { Jumbotron } from 'react-bootstrap';
-import NavBar from './components/NavBar.jsx';
-import { Parallax } from 'react-parallax';
-import LoginSubmissionForm from './components/LoginSubmissionForm.jsx';
 import SignupSubmissionForm from './components/SignupSubmissionForm.jsx';
+
+// Library components
+import { Button, Jumbotron } from 'react-bootstrap';
+import { Parallax } from 'react-parallax';
 import Modal from 'react-modal';
-import LoadingText from './components/LoadingText.jsx';
-import Footer from './components/Footer.jsx';
-import { Button } from 'react-bootstrap';
-import TiTimes from 'react-icons/lib/ti/times';
 
 const SERVER_URL = "http://127.0.0.1:3000";
 
-
+// Refactor with modals
 const customStyles = {
   content : {
     top                   : '50%',
@@ -39,61 +45,51 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: [],
-      query: '',
       data: [],
-      searchMode: "Loose",
-      username: null,
-      userid: null,
-      loggedIn: false,
-      userFavorites: [],
-      view: 'home',
-      modalLogin: false,
-      modalIsOpen: true,
-      modalSignup: false,
       failLogin: '',
       failSignup: '',
-      tags: DEFAULT_TAGS,
+      items: [],
       loadingText: false,
-      randomTrivia: "Did you know..."
+      loggedIn: false,
+      modalLogin: false,
+      modalSignup: false,
+      query: '',
+      searchMode: "Loose",
+      randomTrivia: "Did you know...",
+      tags: DEFAULT_TAGS,
+      userFavorites: [],
+      userid: null,
+      username: null,
+      view: 'home',
 
     }
 
-    this.setStore = this.setStore.bind(this);
-    this.onSearchHandler = this.onSearchHandler.bind(this);
-    this.onSearchHandler2 = this.onSearchHandler2.bind(this);
-    this.onLoginHandler = this.onLoginHandler.bind(this);
-    this.onSignupHandler = this.onSignupHandler.bind(this);
-    this.onFavoriteHandler = this.onFavoriteHandler.bind(this);
-    this.modalLogin = this.modalLogin.bind(this);
-    this.modalSignup = this.modalSignup.bind(this);
-    this.openModal = this.openModal.bind(this);
-    this.closeLogin = this.closeLogin.bind(this);
-    this.closeSignup = this.closeSignup.bind(this);
+    // Method bindings
+    // this.closeLogin = this.closeLogin.bind(this);
+    // this.closeSignup = this.closeSignup.bind(this);
     this.handleTagAdd = this.handleTagAdd.bind(this);
     this.handleTagDelete = this.handleTagDelete.bind(this);
+    this.modalLogin = this.modalLogin.bind(this);
+    this.modalSignup = this.modalSignup.bind(this);
+    this.onSearchHandler = this.onSearchHandler.bind(this);
+    this.onSearchHandler2 = this.onSearchHandler2.bind(this);
+    this.onFavoriteHandler = this.onFavoriteHandler.bind(this);
+    this.onLoginHandler = this.onLoginHandler.bind(this);
+    this.onSignupHandler = this.onSignupHandler.bind(this);
+    // this.openModal = this.openModal.bind(this);
     this.randomTrivia = this.randomTrivia.bind(this);
+    this.setStore = this.setStore.bind(this);
 
     this.randomTrivia();
   }
 
+  // Sets random trivia on footer.
   randomTrivia() {
     spoonacularTrivia((data) => {
       this.setState({randomTrivia: "Did you know... " + data.text});
     });
   }
 
-  openModal() {
-    this.setState({modalIsOpen: true});
-  }
-
-  closeLogin() {
-    this.setState({modalLogin: false, failLogin: ''})
-  }
-
-  closeSignup() {
-    this.setState({modalSignup: false, failSignup: ''})
-  }
 
   setStore(state) {
     this.setState(state)
@@ -103,7 +99,6 @@ class App extends React.Component {
     event.preventDefault();
     var favorites = this.state.userFavorites.slice();
     favorites.push(data);
-    console.log('FAVORITE DATA:', data);
     this.setState({
       userFavorites: favorites
     });
@@ -120,11 +115,9 @@ class App extends React.Component {
       return;
     }
     $.post('/login', userInput, (data) => {
-      console.log('user data: ', data);
       if(data.length !== 0) {
         if(data[0].password === userInput.password) {
           $.post('/useringredients', {userid:data[0].id}, (data) => {
-            console.log('INGREDIENTS: ', data);
             var tags = this.state.tags.slice()
             var id = tags.length+1
             data.forEach( (ingredient) => {
@@ -188,11 +181,9 @@ class App extends React.Component {
       return tag.text
     })
     options.ingredients = tags;
-    console.log('TAGS: ', tags);
     var queryArray = options.ingredients;
     this.setStore({loadingText: true});
     searchSpoonacular(options, (matches) => {
-      console.log("Searching Spoonacular....");
       var data = [];
       if (this.state.searchMode === "Strict") {
         for (var n = 0; n < matches.length; n++) {
@@ -203,14 +194,12 @@ class App extends React.Component {
       } else if (this.state.searchMode === "Loose") {
         data = matches;
       }
-      console.log('Spoonacular found the following recipes!\n', data);
       this.setStore({data: data, loadingText: false});
     });
   }
 
   onSearchHandler(e) {
     e.preventDefault();
-    console.log('Here is your search query: ', this.state.query);
     var options = {};
 
     options.ingredients = this.state.query.split(", ");
@@ -269,7 +258,6 @@ class App extends React.Component {
   }
 
   modalSignup() {
-    console.log('modal sign up');
     if(this.state.loggedIn) {
       this.setState({
         loggedIn: false,
@@ -285,7 +273,6 @@ class App extends React.Component {
   }
 
   handleTagAdd(tag) {
-    console.log('tag add: ', tag);
     tag = tag + '  '
     var tags = this.state.tags.slice()
     var tagId = tags.length+1
@@ -296,12 +283,10 @@ class App extends React.Component {
     if(this.state.userid) {
       $.post('/ingredients', ingredients, (data) => {} );
     }
-    console.log('TAGS: ',tags);
     this.setStore({tags:tags})
   }
 
   handleTagDelete(tag) {
-    console.log('TAG to delete: ', tag);
     var tags = this.state.tags.slice()
     tags.splice(tag,1)
     this.setStore({tags:tags})
@@ -335,8 +320,8 @@ class App extends React.Component {
           // onAfterOpen={this.afterOpenModal} this is here to show that this onAfterOpen method is available
           // onRequestClose={this.closeModal} this is here to show that this onAfterOpen method is available
           contentLabel="login"
-        > <a className="close-button" onClick={this.closeLogin}><TiTimes /></a>
-          <LoginSubmissionForm onLoginHandler={this.onLoginHandler}/>
+        >
+          <LoginSubmissionForm setStore={this.setStore} onLoginHandler={this.onLoginHandler}/>
           <div id='login-fail'>{this.state.failLogin}</div>
         </Modal>
 
@@ -345,20 +330,14 @@ class App extends React.Component {
           contentLabel="signup"
           style={customStyles}
         >
-          <a className="close-button" onClick={this.closeSignup}><TiTimes /></a>
-          <SignupSubmissionForm onSignupHandler={this.onSignupHandler}/>
+
+          <SignupSubmissionForm setStore={this.setStore} onSignupHandler={this.onSignupHandler}/>
           <div id='signup-fail'>{this.state.failSignup}</div>
         </Modal>
         <Search clickHandler={this.onSearchHandler2} handleTagDelete={this.handleTagDelete} handleTagAdd={this.handleTagAdd} tags={this.state.tags} setStore={this.setStore} appState={this.state}/>
         {(this.state.loadingText) ? (<LoadingText />) : null}
         <RecipeList data={this.state.data} onFavoriteHandler={this.onFavoriteHandler}/>
       </div>
-    </div>);
-  }
-
-  testComponents() {
-    return (<div>
-      <_Test /> {/*Feel free to remove me!*/}
     </div>);
   }
 
